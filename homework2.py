17f5a2e6176e4efb667d86a3f29d491c2199e257
Alexandr-U8 <utkin88nya@yandex.ru>
2024-07-25 21:48:30 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kolichestvo_vypolnenyh_dz = 12
+kolichestvo_zatrachenyh_chasov = 1.5
+nazvanie_kyrsa = "Python"
+vremia_na_odno_zadanie = kolichestvo_zatrachenyh_chasov / kolichestvo_vypolnenyh_dz
+print('Курс:',nazvanie_kyrsa,'всего задач:',kolichestvo_vypolnenyh_dz,',затрачено часов:',kolichestvo_zatrachenyh_chasov,',среднее время выполнения',vremia_na_odno_zadanie,'часа.')
